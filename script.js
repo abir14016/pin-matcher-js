2effdacc55be038c3e27908ff53fcc9d17419a8e
abir14016 <abir14016@gmail.com>
2022-02-15 16:40:12 +0600

    pin matcher validated

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ function getPin(){
 document.getElementById('pin-generator-button').addEventListener('click',function(){
     getPin();
 });
+// handle buttons 
+document.getElementById('buttons').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    const numberDisplayInput = document.getElementById('numbers-display');
+    if(isNaN(number)){ 
+        if(number == 'C'){
+            numberDisplayInput.value = '';
+        }   
+    }
+    else{
+        previousNumber = numberDisplayInput.value;
+        const newNumber = previousNumber + number;
+        numberDisplayInput.value = newNumber;
+    }
+});
+//handle submit button
+document.getElementById('submit-button').addEventListener('click',function(){
+    const generatedPin = document.getElementById('pin-display').value;
+    const typedNumber = document.getElementById('numbers-display').value;
+    if(generatedPin == typedNumber){
+        document.getElementById('notify-succed').style.display = 'block';
+        document.getElementById('notify-failed').style.display = 'none';
+    }
+    else{
+        document.getElementById('notify-succed').style.display = 'none';
+        document.getElementById('notify-failed').style.display = 'block';
+        const tryLeftField = document.getElementById('try-left');
+        let tryLeft = parseInt(tryLeftField.innerText);
+        tryLeft = tryLeft-1;
+        tryLeftField.innerText = tryLeft;
+        if(tryLeft<1){
+            document.getElementById('submit-button').style.display = 'none';
+            document.getElementById('try-limit-cross').style.display = 'block';
+            document.getElementById('notify-failed').style.display = 'none';
+        }
+    }
+})
